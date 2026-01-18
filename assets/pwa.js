163f559ce8f7e3860013f323ec393b5a6cbcb60e
hungnq131193-ux <hungnq131193@gmail.com>
2026-01-18 11:07:28 +0700

    Delete assets/pwa.js

--- assets/pwa.js
+++
@@ -1,8 +0,0 @@
-    // Đăng ký PWA Service Worker
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('./sw.js')
-                .then(reg => console.log('Service Worker đã sẵn sàng!', reg))
-                .catch(err => console.log('Lỗi Service Worker:', err));
-        });
-    }
