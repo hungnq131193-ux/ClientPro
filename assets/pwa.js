07a0369549007539161c2f7925d44ad31b6b9086
hungnq131193-ux <hungnq131193@gmail.com>
2026-01-27 11:03:19 +0700

    Add files via upload

--- assets/pwa.js
+++ assets/pwa.js
@@ -14,7 +14,7 @@
   // IMPORTANT (GitHub Pages / aggressive HTTP caches):
   // Register SW with a build query so browsers reliably fetch the latest sw.js.
   // Keep this in sync with sw.js VERSION.
-  var SW_BUILD = 'v4.4.4_grid_fix';
+  var SW_BUILD = 'v4.4.5_small_theme';
 
   function sendSkipWaiting(sw) {
     try {
